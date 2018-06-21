7050760a5f6b86bfab7142485c461238062b82fb
des-yogi <nuevo-design@mail.ru>
2018-06-21 19:11:26 +0300

    index markup

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,24 @@
-// Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+
+  $('.team__slider').owlCarousel({
+    center: true,
+    items: 1,
+    loop: true,
+    margin: 48,
+    // autoWidth: true,
+    nav: true,
+    dots: false,
+    responsive:{
+      768: {
+        items: 2
+      },
+      1200: {
+        items: 3
+      }
+    }
+  });
+
+});
 
 // Изоляция без jQuery
 // (function(){
